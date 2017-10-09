41645e6fcf4c45132f9d42ec4d991d98fceba8d6
Hassanin Ahmed <sas1ni69@gmail.com>
2017-10-09 19:14:27 +0800

    manage dotenv

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if Rails.env.development?
+  Dotenv::Railtie.load
+end
 
 module Luckydraw
   class Application < Rails::Application
